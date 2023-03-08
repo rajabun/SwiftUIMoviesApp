68ddac9bc5140c5e6141e69264b7d1bacbd053f6
Muhammad Rajab <Muhammad.Rajab@siloamhospitals.com>
2023-03-08 12:55:09 +0700

    Initial Commit

---
+++ SwiftUIMoviesApp/SwiftUIMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoviesAppApp.swift
+//  SwiftUIMoviesApp
+//
+//  Created by Rajab on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
